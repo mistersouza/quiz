7db271f9507cb507b907a3fca7d958a83a266a81
mistersouza <>
2023-10-17 07:49:17 +0000

    Add questons data structure

---
+++ assets/scripts/questions.js
@@ -0,0 +1,37 @@
+const questions = [
+    {
+        question: "JavaScript is a ___ -side programming language.",
+        a: "Client",
+        b: "Server",
+        c: "Both",
+        d: "None",
+        correct: "c"
+    }, {
+        question: "Which of the following will write the message “Hello DataFlair!” in an alert box?",
+        a: "alertBox(“Hello DataFlair!”);",
+        b: "alert(Hello DataFlair!);",
+        c: "msgAlert(“Hello DataFlair!”);",
+        d: "alert(“Hello DataFlair!”);",
+        correct: "d"
+    }, {
+        question: "How do you find the minimum of x and y using JavaScript?",
+        a: "min(x,y);",
+        b: "Math.min(x,y)",
+        c: "Math.min(xy)",
+        d: "min(xy);",
+        correct: "b"
+    }, {
+        question: "Which JavaScript label catches all the values, except for the ones specified?",
+        a: "catch",
+        b: "label",
+        c: "try",
+        d: "default",
+        correct: "d"
+    }, {
+        question: "What will the code return? Boolean(3 < 7)",
+        a: "NaN",
+        b: "false",
+        c: "true",
+        d: "SyntaxError",
+        correct: "c"
+    }];
